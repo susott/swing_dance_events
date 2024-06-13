5787c3f7a9b7bb87b2243caaf8cad02992325ec9
Susanne <37674058+susott@users.noreply.github.com>
2024-06-13 14:48:57 +0200

    add a Event model spec

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,17 @@
 class Event < ApplicationRecord
   normalizes :website, with: -> website { website.strip.downcase }
-  # strip title, validate title length
+
+  validates :title, length: { minimum: 4, maximum: 200 }, presence: true
+  validates :dance_types, inclusion: { in: %w(lindy_hop balboa solo_jazz), message: "%{value} is not supported" }
+  validates :start_date, :end_date, presence: true
+  # TODO [:lindy, :collegiate_shag, :sankt_luis_shag, :blues, :balboa, west_coast, :charleston, :solo_jazz]
+  validate :start_date_before_end_date
   # website should be an URI thingy
   # description can be empty
-  # dance_types should be [:lindy, :collegiate_shag, :sankt_luis_shag, :blues, :balboa, west_coast, :charleston, :solo_jazz]
+
+  private
+
+  def start_date_before_end_date
+    errors.add(:end_date, "Event can not end before it starts - please correct") if start_date.after?(end_date)
+  end
 end
---
+++ spec/models/event_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe Event do
+  it 'events can be created' do   #
+    event = Event.new(title: 'Some Event', start_date: '12 Jun 2025'.to_datetime, end_date: '14 Jun 20205'.to_datetime)
+
+    expect(event).to be_valid
+  end
+
+  it 'events end_date can not be before start_date' do  
+    event = Event.new(title: 'Some Event', start_date: '14 Jun 2025'.to_datetime, end_date: '12 Jun 2025'.to_datetime)
+    expect(event).to be_invalid
+  end
+end
