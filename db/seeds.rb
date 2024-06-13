87592e4b2a83ebd23822b45eb78765edfdc6da85
Susanne <37674058+susott@users.noreply.github.com>
2024-06-13 13:50:13 +0200

    add seed for Event

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,14 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+['first', 'second', 'third'].each do |num|
+  Event.find_or_create_by!(title: "My #{num} event",
+                           start_date: Date.today + 14.day,
+                           end_date: Date.today + 16.days,
+                           website: 'https://www.example.com',
+                           description: 'awesome event',
+                           country: 'Germany',
+                           city: 'Berlin',
+                        )
+end
