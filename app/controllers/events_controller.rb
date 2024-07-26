a8060214f57ca1e880de64cda4f15c79498b1130
Susanne <37674058+susott@users.noreply.github.com>
2024-07-26 15:13:04 +0200

    fix dance_type and add filter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -25,6 +25,7 @@ class EventsController < ApplicationController
     @events = Event.upcoming.where(nil) # creates an anonymous scope
     @events = @events.filter_by_city(filter_params[:city]) if params[:city].present?
     @events = @events.filter_by_country(filter_params[:country]) if params[:country].present?
+    @events = @events.filter_by_dance_types(filter_params[:dance_type]) if params[:dance_type].present?
   end
 
   private
@@ -35,6 +36,6 @@ class EventsController < ApplicationController
     end
 
     def filter_params
-      params.slice(:city, :country, :id)
+      params.slice(:city, :country, :id, :dance_type)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,22 +1,22 @@
 class Event < ApplicationRecord
+  DANCE_TYPES = %w(lindy_hop balboa solo_jazz collegiate_shag sankt_luis_shag blues west_coast charleston solo_jazz boogie_woogie).freeze
+
   normalizes :website, with: -> website { website.strip.downcase }
 
-  DANCE_TYPES = %w(lindy_hop balboa solo_jazz, collegiate_shag, sankt_luis_shag, blues, west_coast, charleston, solo_jazz boogie_woogie).freeze
-
   validates :title, length: { minimum: 4, maximum: 200 }, presence: true
-  validates :dance_types, inclusion: { in: DANCE_TYPES, message: "%{value} is not supported" }
-  validates :start_date, :end_date, presence: true
+  validates :start_date, :end_date, :description, presence: true
   validate :start_date_before_end_date
+  validate :supported_dance_types
 
   scope :upcoming, -> { where("end_date > ?", Date.today) }
   scope :past, -> { where("end_date <= ?", Date.today) }
 
   scope :filter_by_city, -> (city) { where(city: city) }
   scope :filter_by_country, -> (country) { where(country: country) }
-  # scope :filter_by_dance_types, -> ()
+  scope :filter_by_dance_types, -> (dance) { where('DANCE_TYPES && array[?]', dance)}
 
   # website should be an URI thingy
-  # description can be empty
+  # description can be empty ?
 
   # currently only used for comand line
   def upcoming?
@@ -27,5 +27,9 @@ class Event < ApplicationRecord
 
   def start_date_before_end_date
     errors.add(:end_date, "Event can not end before it starts.") if start_date.after?(end_date)
+  end
+
+  def supported_dance_types
+    errors.add(:dance_types, "Unsupported dance type") if (dance_types - DanceType::DANCE_TYPES).present?
   end
 end
