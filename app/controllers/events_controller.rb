a5f36288b217e4c952381a057ccd27b5910f14b9
Susanne <37674058+susott@users.noreply.github.com>
2024-07-26 11:27:07 +0200

    add basic features

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,40 @@
 class EventsController < ApplicationController
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def show
+    @event = Event.find(filter_params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render html: helpers.tag.strong('This page does not exist')
   end
 
   def index
-    @events = Event.all
+    # see https://www.justinweiss.com/articles/search-and-filter-rails-models-without-bloating-your-controller/
+    @events = Event.upcoming.where(nil) # creates an anonymous scope
+    @events = @events.filter_by_city(filter_params[:city]) if params[:city].present?
+    @events = @events.filter_by_country(filter_params[:country]) if params[:country].present?
   end
+
+  private
+    def event_params
+      params.require(:event).permit(
+        :title, :description, :dance_types, :start_date, :end_date, :website, :sign_up_start_date, :event_email, :country, :city
+      )
+    end
+
+    def filter_params
+      params.slice(:city, :country, :id)
+    end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,17 +1,31 @@
 class Event < ApplicationRecord
   normalizes :website, with: -> website { website.strip.downcase }
 
+  DANCE_TYPES = %w(lindy_hop balboa solo_jazz, collegiate_shag, sankt_luis_shag, blues, west_coast, charleston, solo_jazz boogie_woogie).freeze
+
   validates :title, length: { minimum: 4, maximum: 200 }, presence: true
-  validates :dance_types, inclusion: { in: %w(lindy_hop balboa solo_jazz), message: "%{value} is not supported" }
+  validates :dance_types, inclusion: { in: DANCE_TYPES, message: "%{value} is not supported" }
   validates :start_date, :end_date, presence: true
-  # TODO [:lindy, :collegiate_shag, :sankt_luis_shag, :blues, :balboa, west_coast, :charleston, :solo_jazz]
   validate :start_date_before_end_date
+
+  scope :upcoming, -> { where("end_date > ?", Date.today) }
+  scope :past, -> { where("end_date <= ?", Date.today) }
+
+  scope :filter_by_city, -> (city) { where(city: city) }
+  scope :filter_by_country, -> (country) { where(country: country) }
+  # scope :filter_by_dance_types, -> ()
+
   # website should be an URI thingy
   # description can be empty
+
+  # currently only used for comand line
+  def upcoming?
+    end_date > Date.today
+  end
 
   private
 
   def start_date_before_end_date
-    errors.add(:end_date, "Event can not end before it starts - please correct") if start_date.after?(end_date)
+    errors.add(:end_date, "Event can not end before it starts.") if start_date.after?(end_date)
   end
 end
