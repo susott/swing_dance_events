32c813b5a68d6a792ec1fce6d7de61c9933e6684
Susanne <37674058+susott@users.noreply.github.com>
2024-08-20 13:46:29 +0200

    add view specs for events index and events show

--- spec/views/events/index.html.slim_spec.rb
+++ spec/views/events/index.html.slim_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe 'events/index.html.slim', type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'show the filter form' do
+    render
+    expect(rendered).to match(/<input type="submit" name="commit"/)
+    expect(rendered).to include('Filter by City')
+    expect(rendered).to include('Filter by Dance')
+  end
+
+  it 'shows text if the event does not exist' do
+    render
+    expect(rendered).to include 'No events found. Please add some.'
+  end
+
+  it 'shows some details about an event' do
+    @events = create_list(:event, 2)
+    render
+
+    expect(rendered).to include @events.first.title
+    expect(rendered).to include @events.first.city
+    expect(rendered).to include @events.first.start_date.strftime('%B %d, %Y')
+
+    expect(rendered).to include @events.second.title
+    expect(rendered).to include @events.second.city
+    expect(rendered).to include @events.second.start_date.strftime('%B %d, %Y')
+  end
 end
--- spec/views/events/show.html.slim_spec.rb
+++ spec/views/events/show.html.slim_spec.rb
@@ -3,16 +3,16 @@ require 'rails_helper'
 RSpec.describe 'events/show.html.slim', type: :view do
   it 'shows some text if the event does not exist' do
     render
-    expect(rendered).to match 'Sorry. This event can not be found.'
+    expect(rendered).to include 'Sorry. This event can not be found.'
   end
 
   it 'shows some details about an event' do
     @event = build(:event)
     render
 
-    expect(rendered).to match @event.title
-    expect(rendered).to match @event.description
-    expect(rendered).to match @event.start_date.strftime('%B %d, %Y')
-    expect(rendered).to match @event.end_date.strftime('%B %d, %Y')
+    expect(rendered).to include @event.title
+    expect(rendered).to include @event.description
+    expect(rendered).to include @event.start_date.strftime('%B %d, %Y')
+    expect(rendered).to include @event.end_date.strftime('%B %d, %Y')
   end
 end
