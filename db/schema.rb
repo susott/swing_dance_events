6d208d5acf597e5cf5e9bb268fa7eda7f81a89ee
Susanne <37674058+susott@users.noreply.github.com>
2024-11-06 12:53:20 +0100

    fix / recreate schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -37,23 +37,4 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_06_094008) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
 end
